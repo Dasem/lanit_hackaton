be81a8b20006d1067527f60baa09e85c2beab81e
Kostyrnaya <Kostyrnaya@lanit.ru>
2019-11-02 16:37:13 +0500

    БД

--- userService.py
+++ userService.py
@@ -1,8 +1,13 @@
 import dao;
 
 def add(user_id, city, lunch_id):
-    dao.getCursor().execute("""INSERT INTO users (user_id, city, lunch_id) VALUES (?, ?, ?)""", (user_id, city, lunch_id))
+    dao.getCursor().execute("""INSERT INTO users (user_id, city, lunch_id) VALUES (?, ?, ?)""",
+                            (user_id, city, lunch_id))
     dao.get_connection().commit()
 
+
 def findById(userId):
-    return dao.getCursor().execute("""SELECT * FROM users WHERE user_id = ?""", [int(userId)]).fetchone()
+    result = dao.getCursor().execute("""SELECT user_id, city, lunch_id FROM users WHERE user_id = ?""",
+                                     [int(userId)]).fetchone()
+    user = {'user_id': result[0], 'city': result[1], 'lunch_id': result[2]}
+    return user
